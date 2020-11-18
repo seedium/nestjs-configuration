44c338f498af77cc44ab84015a2ea0d7865ae5bc
Kostya Zgara <k.zgara@seedium.io>
2020-11-18 17:49:21 +0200

    feat(): set `DotenvConfigLoader` as loader by default
    Closes #6

--- lib/config.module.ts
+++ lib/config.module.ts
@@ -3,9 +3,32 @@ import { DynamicModule, FactoryProvider } from '@nestjs/common';
 import type { O } from 'ts-toolbelt';
 import { Config } from './config.provider';
 import { IConfigLoader, IConfigOptions } from './interfaces';
+import { DotenvConfigLoader } from './loaders';
 
 export class ConfigModule {
-  static forFoot(
+  static forFoot({
+    pattern = '*.ts',
+    ...options
+  }: O.Optional<IConfigOptions, 'pattern'>): DynamicModule {
+    const configProvider: FactoryProvider = {
+      provide: Config,
+      useFactory: async () =>
+        await defer(async () => {
+          const config = new Config(new DotenvConfigLoader(), {
+            pattern,
+            ...options,
+          });
+          await config.load();
+          return config;
+        }).toPromise(),
+    };
+    return {
+      module: ConfigModule,
+      exports: [configProvider],
+      providers: [configProvider],
+    };
+  }
+  static forFeature(
     configLoader: IConfigLoader,
     { pattern = '*.ts', ...options }: O.Optional<IConfigOptions, 'pattern'>,
   ): DynamicModule {
--- test/config-module.spec.ts
+++ test/config-module.spec.ts
@@ -2,7 +2,12 @@ import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import { Test } from '@nestjs/testing';
-import { Config, ConfigModule, IConfigLoader } from '../lib';
+import {
+  Config,
+  ConfigModule,
+  DotenvConfigLoader,
+  IConfigLoader,
+} from '../lib';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -17,11 +22,20 @@ describe('ConfigModule', () => {
   afterEach(() => {
     sinon.restore();
   });
-  it('should provide', async () => {
+  it('should provide `DotenvConfigLoader` by default', async () => {
     const testModule = await Test.createTestingModule({
-      imports: [ConfigModule.forFoot(new TestLoader(), { path: 'test' })],
+      imports: [ConfigModule.forFoot({ path: 'test' })],
     }).compile();
     const config = testModule.get(Config);
     expect(config).instanceOf(Config);
+    expect(config).property('_loader').instanceOf(DotenvConfigLoader);
+  });
+  it('should provide custom provide', async () => {
+    const testModule = await Test.createTestingModule({
+      imports: [ConfigModule.forFeature(new TestLoader(), { path: 'test' })],
+    }).compile();
+    const config = testModule.get(Config);
+    expect(config).instanceOf(Config);
+    expect(config).property('_loader').instanceOf(TestLoader);
   });
 });
